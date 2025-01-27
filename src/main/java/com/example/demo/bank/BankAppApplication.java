992eff32a636f4be573fd3568ceaab0e61c17f2c
T3rm1D0 <gelmanas.dovydas@gmail.com>
2025-01-27 14:14:32 +0200

    Updated project with latest changes

--- src/main/java/com/example/demo/bank/BankAppApplication.java
+++ src/main/java/com/example/demo/bank/BankAppApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@SpringBootApplication
+
 @RestController
 @RequestMapping("/api/accounts")
 public class BankAppApplication {
